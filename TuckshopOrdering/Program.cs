ff3b6ee2f89c506dd0c69ae3c031dabe5817fa6b
Aqua7710 <purdonwill@gmail.com>
2024-04-07 17:59:11 +1200

    I updated the colours on the website, added the logo, added modal in replacement of the pop-up forms, and adjusted the navigation controls for the orders carousel.

--- TuckshopOrdering/Program.cs
+++ TuckshopOrdering/Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Menus}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
