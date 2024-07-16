6ebe2e5becd2d121d077699f0eed8662c3dede41
Aqua7710 <purdonwill@gmail.com>
2024-07-16 22:18:15 +1200

    final model validations

--- TuckshopOrdering/Models/Order.cs
+++ TuckshopOrdering/Models/Order.cs
@@ -7,11 +7,15 @@ namespace TuckshopOrdering.Models
         public int OrderID { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime PickupDate { get; set; }
+        [StringLength(50)]
         public string studentName { get; set; }
+        [Range(1, 34)]
         public float roomNumber { get; set; }
         public string Status { get; set; } = "Pending"; // default status
+        [EmailAddress]
         public string? email { get; set; }
         public string orderComplete { get; set; }
+        [StringLength(100)]
         public string? note { get; set; }
         public ICollection<FoodOrder> FoodOrders { get; set; } // 1 order can contain many food orders
     }
